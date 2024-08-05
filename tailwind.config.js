1f4c76f50117e8ad68fc3bbe58ee249caf343a58
Alexis G <alexis.gontier03@gmail.com>
2024-08-05 22:39:44 +0200

    ajout de fichier

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,25 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        'poppins': ['Poppins', 'sans-serif'],
+      },
+      colors: {
+        'white': '#EEEEF0',
+        'gray': '#B2B3BD',
+        'violet': '#D29DFF',
+        'bg-page': '#130F19',
+        'bg-nav': 'rgba(17, 17, 19, 0.85)',
+        'bg-card': '#1C1524',
+        'bg-tech': '#747474',
+        'border-colors': 'rgba(57, 58, 64, 0.50)',
+        'shadow-nav': 'rgba(122, 63, 183, 0.10)',
+        'green': '#37744E',
+        'banner-purple': '#6D0094',
+        'btn-purple': '#8E4EC6',
+      }
+    },
   },
   plugins: [],
 }
